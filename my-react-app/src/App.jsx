9b6d4f51fb815fa1468d7454a43fd245a5f531f7
Shubh Chawda <shubh.chawda@student.manchester.ac.uk>
2025-12-28 16:56:03 +0000

    Add initial routing and clean App.jsx structure

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,23 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './Components/navigation-bar.jsx';
-import AboutRestaurant from './pages/AboutRestraunt.jsx'; 
-import MainPage from './pages/mainPage';
-
-
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/navigation-bar.jsx";
+import MainPage from "./pages/MainPage.jsx";
+import AboutRestaurant from "./pages/AboutRestraunt.jsx";
 
 export default function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/about' element={<AboutRestaurant />} />
-       
-        
-
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutRestaurant />} />
       </Routes>
     </Router>
   );
 }
+
