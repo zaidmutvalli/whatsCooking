d989b7b4a7005ec4748583e4813c4c73e52f6f4d
JoshA380 <Joshuaantcliffe380@gmail.com>
2025-12-29 17:07:28 +0000

    merged signup_page_combine_branches

--- my-react-app/src/pages/loginPage.jsx
+++ my-react-app/src/pages/loginPage.jsx
@@ -1,9 +1,45 @@
-import React from "react";
-import Login from "../Components/login";
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 
-import "../styles/loginPage.css";
+export default function LogInPage() {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    });
 
-export default function LoginPage(){
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost/whatscooking/backend/login.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        });
+
+        const result = await response.json();
+
+        if (result.status === "success") {
+            alert("Log In Successful!");
+            navigate("/");  
+        } else {
+            alert(result.message); 
+        }
+        } catch (error) {
+            alert("An error occurred. Please try again.");
+        }
+    }
 
     return (
         <div className="wrapper">
