bd24ba421b8a88bb9f0417f2fda133b87631208f
JoshA380 <Joshuaantcliffe380@gmail.com>
2026-01-31 11:28:31 +0000

    Now the maps display a default marker

--- my-react-app/src/Components/map.jsx
+++ my-react-app/src/Components/map.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {APIProvider, Map ,} from '@vis.gl/react-google-maps';
+import {APIProvider, AdvancedMarker, Map ,} from '@vis.gl/react-google-maps';
 
 
 
@@ -7,6 +7,11 @@ import {APIProvider, Map ,} from '@vis.gl/react-google-maps';
 
 export default function DisplayMap({lat, lng}) {
     const myAPIkey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY || import.meta.env.VITE_GOOGLE_API_KEY;
+    const mapID= import.meta.env.VITE_GOOGLE_MAP_ID || import.meta.env.VITE_GOOGLE_MAP_ID;
+   
+    
+      
+      
     
     return (
       <APIProvider apiKey={myAPIkey} onLoad={()=>console.log("Google Maps API loaded")}>
@@ -14,8 +19,12 @@ export default function DisplayMap({lat, lng}) {
       defaultZoom={13}
       defaultCenter={ { lat: 53.478559, lng: -2.242610 } }
       disableDefaultUI={true}
+      mapId={mapID}
       
       >
+      <AdvancedMarker position={{lat: 53.478559, lng: -2.242610}} />
+
+
    </Map>
         
         
