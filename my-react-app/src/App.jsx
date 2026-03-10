85bc6ebf6466d1b255ab2e2fce977d8688062a1b
Amy <amy.johnson-9@student.manchester.ac.uk>
2026-03-10 15:34:38 +0000

    ignore

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,13 +7,10 @@ import SignUpPage from './pages/SignUpPage';
 import LogInPage from './pages/loginPage';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
-<<<<<<< HEAD
-=======
 import PlacesPage from './pages/PlacesPage';
 import SocialPage from './pages/SocialPage';
 import TrendingPage from './pages/TrendingPage';
 import UserSettings from './pages/user-settings';
->>>>>>> 69f121da4c7145f41742899d71a4ce695fa5b325
 
 function App() {
   return (
@@ -27,14 +24,10 @@ function App() {
         <Route path='/logIn' element={<LogInPage />} />
         <Route path='/forgotPassword' element={<ForgotPassword />} />
         <Route path='/resetPassword' element={<ResetPassword />} />
-<<<<<<< HEAD
-
-=======
         <Route path='/places' element={<PlacesPage />} />
         <Route path='/social' element={<SocialPage />} />
         <Route path='/trending' element={<TrendingPage />} />
         <Route path="/user-settings" element={<UserSettings />} />
->>>>>>> 69f121da4c7145f41742899d71a4ce695fa5b325
       </Routes>
     </Router>
   );
