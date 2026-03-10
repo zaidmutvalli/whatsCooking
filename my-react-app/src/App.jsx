394fc631056bb1b511b16abee819d40d763fc7cd
Amy <amy.johnson-9@student.manchester.ac.uk>
2026-03-10 15:05:07 +0000

    API keys

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
-
-import NavBar from "./Components/navigation-bar";
-import MainPage from "./pages/mainPage"; 
-
-import AboutRestaurant from "./pages/AboutRestraunt"; 
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './Components/navigation-bar';
 import AboutRestaurant from './pages/AboutRestraunt';
@@ -18,19 +6,12 @@ import SignUpPage from './pages/SignUpPage';
 import LogInPage from './pages/loginPage';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
->>>>>>> remotes/origin/signup_page_combine_branches
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
-<<<<<<< HEAD
-        {/* This tells the app: When at '/', show MainPage */}
-        <Route path="/" element={<MainPage />} />
-        {/* This tells the app: When at '/about', show AboutRestaurant */}
-        <Route path="/about" element={<AboutRestaurant />} />
-=======
         <Route path='/' element={<MainPage />} />
         <Route path='/about' element={<AboutRestaurant />} />
         <Route path='/signUp' element={<SignUpPage />} />
@@ -38,7 +19,6 @@ function App() {
         <Route path='/forgotPassword' element={<ForgotPassword />} />
         <Route path='/resetPassword' element={<ResetPassword />} />
 
->>>>>>> remotes/origin/signup_page_combine_branches
       </Routes>
     </Router>
   );
